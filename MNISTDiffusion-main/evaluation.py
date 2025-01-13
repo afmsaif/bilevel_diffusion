3c3cd5b17034434f9b5dc34e219a7eb9e088c0fe
JenniferquanXiao <55336026+JenniferquanXiao@users.noreply.github.com>
2025-01-12 20:09:11 -0500

    Add files via upload

--- MNISTDiffusion-main/evaluation.py
+++ MNISTDiffusion-main/evaluation.py
@@ -24,45 +24,95 @@ from pytorch_fid.inception import InceptionV3
 
 block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[2048]
 
+from torchmetrics.metric import Metric
 
 
-
-class FIDCalculator:
-    def __init__(self, real_features):
+class FIDCalculator():
+    # def __init__(self, real_features):
+    def __init__(self, device):
+        self.device = device
         # Compute mean and covariance of real features during initialization
-        self.model__ = InceptionV3([block_idx]).cuda()
-        self.model__.eval()
-        real_features = self._process_features(real_features)
-        self.mean_real = real_features.mean(dim=0)
-        self.cov_real = torch.cov(real_features.T)
-        self.trace_real = self.cov_real.trace()
+        self.model__ = InceptionV3([block_idx],normalize_input=False).to(device)
+        self.model__.train()
+        # self.model__.eval()
+
+
+        # Initialize accumulators
+        self.sum_real = torch.zeros(2048, device=device)  # Shape: (D,)
+        self.cov_sum_real = torch.zeros(2048, 2048, device=device)  # Shape: (D, D)
+        self.real_features_num_samples = 0  # Total number of samples
+
+        self.sum_fake = torch.zeros(2048, device=device)  # Shape: (D,)
+        self.cov_sum_fake = torch.zeros(2048, 2048, device=device)  # Shape: (D, D)
+        self.fake_features_num_samples = 0  # Total number of samples
+
+        # self.model__.eval()
+        # real_features = self._process_features(real_features)
+        # self.mean_real = real_features.mean(dim=0)
+        # self.cov_real = torch.cov(real_features.T)
+        # self.trace_real = self.cov_real.trace()
+
+    def clear_fake(self):
+        self.sum_fake = torch.zeros(2048, device=self.device)  # Shape: (D,)
+        self.cov_sum_fake = torch.zeros(2048, 2048, device=self.device)  # Shape: (D, D)
+        self.fake_features_num_samples = 0  # Total number of samples
+
+    def update_real(self,real_features):
+        with torch.enable_grad():
+            real_features = self._process_features(real_features)
+            # print("print(real_features.requires_grad)", real_features.requires_grad)
+            
+            B, D = real_features.shape
+
+            self.sum_real += real_features.sum(dim=0)
+            self.cov_sum_real += real_features.t().mm(real_features)
+            self.real_features_num_samples += B
+
+    def update_fake(self,fake_features):
+        # # self.model__.train()
+        # for para in self.model__.parameters():
+        #     para.requires_grad = True
+        with torch.enable_grad():
+            fake_features = self._process_features(fake_features)
+            # print("print(fake_features.requires_grad)", fake_features.requires_grad)
+            
+            B, D = fake_features.shape
+
+            self.sum_fake += fake_features.sum(dim=0)
+            self.cov_sum_fake += fake_features.t().mm(fake_features)
+            self.fake_features_num_samples += B
 
     def _process_features(self, features):
         """Process input features by passing through the model and squeezing dimensions."""
         features = self.model__(features.repeat(1, 3, 1, 1))[0]
         return features.squeeze(3).squeeze(2)
 
-    def compute(self, gen_features):
+    def compute(self):
         """Compute FID score given generated features."""
-        # Process generated features
-        self.model__.train()
-        gen_features = self._process_features(gen_features)
-        
-        # Compute mean and covariance of generated features
-        mean_fake = gen_features.mean(dim=0)
-        cov_fake = torch.cov(gen_features.T)
-        
-        # Compute FID components
-        a = (self.mean_real.squeeze(0) - mean_fake.squeeze(0)).square().sum(dim=-1)
-        b = self.trace_real + cov_fake.trace()
-        try:
-            c = torch.linalg.eigvals(self.cov_real @ cov_fake).sqrt().real.sum(dim=-1)
-        except Exception as e:
-            print("Error during eigenvalue computation:", e)
-            print("cov_real:", self.cov_real)
-            print("cov_fake:", cov_fake)
-            print("gen_features:", gen_features)
-            print("Product:", self.cov_real @ cov_fake)
+        with torch.enable_grad():
+            self.mean_real = (self.sum_real / self.real_features_num_samples).unsqueeze(0)
+
+            cov_real_num = self.cov_sum_real - self.real_features_num_samples * self.mean_real.t().mm(self.mean_real)
+            self.cov_real = cov_real_num / (self.real_features_num_samples - 1)
+            self.trace_real = self.cov_real.trace()
+
+            self.mean_fake = (self.sum_fake / self.fake_features_num_samples).unsqueeze(0)
+
+            cov_fake_num = self.cov_sum_fake - self.fake_features_num_samples * self.mean_fake.t().mm(self.mean_fake)
+            self.cov_fake = cov_fake_num / (self.fake_features_num_samples - 1)
+            self.trace_fake = self.cov_fake.trace()
+            
+            # Compute FID components
+            a = (self.mean_real.squeeze(0) - self.mean_fake.squeeze(0)).square().sum(dim=-1)
+            b = self.trace_real + self.trace_fake
+            try:
+                c = torch.linalg.eigvals(self.cov_real @ self.cov_fake).sqrt().real.sum(dim=-1)
+            except Exception as e:
+                print("Error during eigenvalue computation:", e)
+                # print("cov_real:", self.cov_real)
+                # print("cov_fake:", self.cov_fake)
+                # print("gen_features:", self.gen_features)
+                # print("Product:", self.cov_real @ self.cov_fake)
 
         return a + b - 2 * c
 
